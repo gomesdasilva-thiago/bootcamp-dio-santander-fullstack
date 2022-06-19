ff7853a93b81846db9841a792304bf4d318067b9
gomesdasilva-thiago <gomesdasilva.thiago@gmail.com>
2022-06-19 16:32:12 -0300

    19/06/2022

---
+++ Módulo II - HTML-CSS3-JS/Estrutura de Dados/index-empty-undefined-null.js
@@ -0,0 +1,15 @@
+//undefined
+
+console.log(abacaxi)
+
+console.log(let abacaxi = "")
+
+console.log(typeof abacaxi)
+
+// empty
+
+let abacaxi = ""
+
+// null
+
+abacaxi = null
---
+++ Módulo II - HTML-CSS3-JS/Estrutura de Dados/index-strings.js
@@ -0,0 +1,53 @@
+// strings
+
+let nome = "João"
+
+let sobrenome = "Pedro"
+
+console.log(typeof nome)
+
+nome.concat(sobrenome)
+
+let concatenado = nome.concat(sobrenome)
+
+typeof concatenado
+
+let exemplo = new String ("blablabla") //não é muito utilizado
+
+typeof exemplo
+
+nome[1] // Indice
+
+concatenado = nome + " " + sobrenome
+
+concatenado = nome + "\n" + sobrenome // pular linha
+
+console.log(concatenado = `${nome} 
+
+${sobrenome}`)
+
+
+concatenado = "\"" // Imprimir aspas
+
+let frase = "Olá tudo bem?"
+
+console.log(frase.split("")) // Separando por letra
+
+console.log(frase.split(" ")) // Separando por espaço / palavra
+
+frase.includes("tudo") // procurar se contém palavra
+
+frase.startsWith("O") // começa com determinada letra
+
+frase.startsWith("R") 
+
+frase.endsWith("?") // termina com determinada letra
+
+// utilizar o ponto após o nome da variável para ver os métodos
+
+let stringModificada = frase.replace(",", "!") //Replace
+
+frase.
+
+
+
